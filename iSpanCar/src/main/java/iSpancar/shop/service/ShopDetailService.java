4130a3c53b5c4e1ae35d46d7e13f3535b9b4b4ac
iitzSammy <sammya0602@gmail.com>
2023-01-12 17:59:00 +0800

    ServiceDone

--- iSpanCar/src/main/java/iSpancar/shop/service/ShopDetailService.java
+++ iSpanCar/src/main/java/iSpancar/shop/service/ShopDetailService.java
@@ -1,47 +1,70 @@
-//package iSpancar.shop.service;
-//
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//import org.hibernate.Session;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import iSpancar.shop.model.ShopDetailBean;
-//import iSpancar.shop.model.ShopDetailDao;
-//@Service
-//@Transactional
-//public class ShopDetailService {
-//
-//	@Autowired
-//	@Qualifier("sDao")
-//	private ShopDetailDao sDao;
-//	
-//	public ShopDetailService() {
-//	}
-//
-//
-//
-//	public List<ShopDetailBean> findAll() {
-//		return sDao.findAllProduct();
-//	}
-//
-//	public ShopDetailBean insert(ShopDetailBean sBean) {
-//		return sDao.insertProduct(sBean);
-//	}
-//
-////	public CompanyBean updateOneCompany(Integer id, String newName) {
-////		return comDao.updateOneCompany(id, newName);
-////	}
-////
-//	public void delete(ShopDetailBean sdb) {
-//		try {
-//			sDao.deleteProduct(sdb);
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//}
+package iSpancar.shop.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.http.Part;
+import javax.sql.rowset.serial.SerialException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import iSpancar.shop.model.ShopDetailBean;
+import iSpancar.shop.model.ShopDetailDao;
+@Service
+@Transactional
+public class ShopDetailService {
+
+	@Autowired
+	@Qualifier("sDao")
+	private ShopDetailDao sDao;
+	
+	public ShopDetailService() {
+	}
+
+
+	public List<ShopDetailBean> findAll() {
+		List<ShopDetailBean> list = sDao.findAllProduct();
+		return list;
+	}
+
+	public ShopDetailBean insert(ShopDetailBean sBean) {
+		sDao.insertProduct(sBean);
+		return null;
+	}
+	
+	public ShopDetailBean update(ShopDetailBean serviceBean) {
+		ShopDetailBean sBean = sDao.updateProduct(serviceBean);
+		return sBean;
+	}
+	
+	public List<ShopDetailBean> findByservice(String serviceno) throws SQLException{
+		List<ShopDetailBean> list = sDao.findByProductno(serviceno);
+		return list;
+	}
+	
+	public ShopDetailBean findImgByService(String service_no) throws SQLException {
+		ShopDetailBean result = sDao.findImgbyproductno(service_no);
+		return result;
+	}
+	
+	public void delete(ShopDetailBean sdb) throws SQLException {
+			sDao.deleteProduct(sdb);
+	}
+	
+	public Blob filetoBlob(InputStream is, long size) throws SerialException, SQLException, IOException {
+		Blob sb = sDao.fileToBlob(is, size);
+		return sb;
+	}
+	
+	public String getFileName(Part part) {
+		String fname = sDao.getFileName(part);
+		return fname;
+	}
+
+}
