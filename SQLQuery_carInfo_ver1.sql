09d7c141775650b13a4a5691a05ff76fcb093921
wu54698 <wu54698@hotmail.com>
2023-02-08 11:40:55 +0800

    2023/02/08 updated new car info multi-image SQL

--- SQLQuery_carInfo_ver1.sql
+++ SQLQuery_carInfo_ver1.sql
@@ -1,6 +1,6 @@
 create table carInfo(
 carNo int primary key identity(1, 1),
-carDealName nvarchar(50),
+carDealName nvarchar(50) foreign key references carDealerInfo(carDealerName),
 accountNumber nvarchar(50),
 carBrand nvarchar(50),
 carName nvarchar(50),
@@ -10,6 +10,14 @@ carDescription nvarchar(MAX),
 announceDate nvarchar(50),
 );
 
+create table carInfoImage(
+imageNo int primary key identity(1000, 1),
+carNo int not null,
+carImage varbinary(MAX),
+);
+
+
+
 alter table carInfo
 alter column carNo int primary key identity(1, 1);
 
@@ -18,6 +26,7 @@ alter column carDealName nvarchar(50) foreign key references carDealerInfo(carDe
 
 
 drop table carInfo;
+drop table carInfoImage;
 
 select* from carInfo;
 select* from carDealerInfo;
